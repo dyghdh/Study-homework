7a7131087d02a2876a710e99cee1268886e8e233
daedcat <dyghdh4@naver.com>
2021-04-13 19:56:05 +0900

    first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("눈을 떠보니 슬라임이 되있었다");
-        System.out.println("1. 주변을 둘러본다 2. 돌을 먹는다");
+        System.out.println("1. 주변을 둘러본다 2. 돌을 먹는다 ");
 
         sc.close();
     }
