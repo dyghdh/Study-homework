3fee159afc38744bc2a9e174f619787803607caf
daedcat <dyghdh4@naver.com>
2021-04-13 19:54:01 +0900

    ㅁㄴㅇ

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("눈을 떠보니 슬라임이 되있었다");
+        System.out.println("1. 주변을 둘러본다 2. 돌을 먹는다");
+
+        sc.close();
     }
 }
