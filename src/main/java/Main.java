9e0b1e08be4e1a5cf39e79faa4e02cd69468da02
daedcat <dyghdh4@naver.com>
2021-04-13 20:21:26 +0900

    first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("눈을 떠보니 슬라임이 되있었다");
         System.out.println("1. 주변을 둘러본다 2. 돌을 먹는다 ");
+        int num = sc.nextInt();
+
+        switch (num){
+            case 1:break;
+            case 2:
+                System.out.println("우걱우걱 단단해지기를 배웠다.");
+        }
+
+        System.out.println("주변은 동굴이다.");
+        System.out.println("1. 안쪽으로 들어간다 2. 밖으로 나간다 ");
+        num = sc.nextInt();
+
+        switch (num){
+            case 1:
+                System.out.println("봉인된 용을 발견했다 ");
+            case 2:
+                System.out.println("밖에나가니 광활한 숲과 햇살이 있다 ");
+        }
 
         sc.close();
     }
